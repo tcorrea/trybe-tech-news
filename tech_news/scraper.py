fa04f9edd2ecacf9964ca6b41c40074c80c4aab7
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-11-08 16:32:29 -0300

    feat: requisito 03 - implementado a função scrape_next_page_link que retorna o link da proxima página

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,7 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    return Selector(html_content).css(".next::attr(href)").get()
 
 
 # Requisito 4
