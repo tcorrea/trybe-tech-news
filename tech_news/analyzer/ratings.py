338ce14af25cbc4a685156fdeb9cbedbcc23481b
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-11-11 11:09:27 -0300

    feat: requisito 10 - lista as top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_5 = find_news()
+    sorted_news = sorted(
+        top_5,
+        # função que organiza pelo comments_count e depois por title se tiver
+        # a mesma quantidade de comments_count
+        key=lambda item: (item["comments_count"], item["title"]),
+        reverse=True,
+    )
+    result = [
+        (single_news["title"], single_news["url"])
+        for single_news in sorted_news[:5]
+    ]
+    print("\nSORTED VAR:", result)
+    return result
 
 
 # Requisito 11
